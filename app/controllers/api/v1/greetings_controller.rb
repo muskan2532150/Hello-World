0eb329825fecc33eb55357d261fddf98dcb968b1
muskan2532150 <rg2532150@gmail.com>
2023-01-21 22:08:15 +0530

    Fix error

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
-    def index
-      greeting = Greeting.order("RANDOM()").first
-      render json: { message: greeting.message }
-    end
+  def index
+    greeting = Greeting.order('RANDOM()').first
+    render json: { message: greeting.message }
+  end
 end
